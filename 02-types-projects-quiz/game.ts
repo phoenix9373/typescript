9b442d8d4fe2fe9993660ec745afbc62b17f6814
Jinwoo <phoenix9373@naver.com>
2021-11-23 18:14:53 +0900

    ✨Feat: 기본 TS 타입 문제 해결

--- 02-types-projects-quiz/game.ts
+++ 02-types-projects-quiz/game.ts
@@ -1,12 +1,40 @@
 /**
  * Let's make a game 🕹
  */
+type Direction = "up" | "down" | "left" | "right";
+type Position = {
+  x: number;
+  y: number;
+};
+function move(direction: Direction, position: Position): Position {
+  switch (direction) {
+    case "up":
+      position.y += 1;
+      return position;
+    case "down":
+      position.y -= 1;
+      return position;
+    case "left":
+      position.x -= 1;
+      return position;
+    case "right":
+      position.x += 1;
+      return position;
+    default:
+      throw Error("unknown direction");
+  }
+}
+const position: Position = {
+  x: 0,
+  y: 0,
+};
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up", position);
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down", position);
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left", position);
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right", position);
 console.log(position); // { x: 0, y: 0}
--- 02-types-projects-quiz/loading.ts
+++ 02-types-projects-quiz/loading.ts
@@ -3,24 +3,34 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  function printLoginState(loadState: ResourceLoadState): void {
+    if (loadState.state === "loading") {
+      console.log("👀 loading...");
+    } else if (loadState.state === "success") {
+      console.log("😃 loaded");
+    } else {
+      console.log("😱 no network");
+    }
+  }
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
