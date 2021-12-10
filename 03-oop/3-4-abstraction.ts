485a0438bc2acd2d50b3fdfdb27fecf34dde43fc
Jinwoo <phoenix9373@naver.com>
2021-12-11 02:08:42 +0900

    🚩 Update: 3-4 추상화 내용 추가(user 부분)

--- 03-oop/3-4-abstraction.ts
+++ 03-oop/3-4-abstraction.ts
@@ -82,25 +82,6 @@
     }
   }
 
-  // maker의 Type은 class(CoffeeMachine) or interface(CoffeeMaker)가 될 수 있다.
-  // 1. CoffeeMachine 클래스로 생성한 instance는 모든 메서드(fillCoffeeBeans, makeCoffee)를 활용할 수 있다.
-  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
-  maker.fillCoffeeBeans(32);
-  maker.makeCoffee(2);
-
-  // 2. Interface로 추상화 => CoffeeMaker Interface로 타입을 지정한 instance
-  // -> maker2는 fillCoffeeBeans에 접근할 수 없다.
-  // -> Interface에 정의된 메서드에만 접근할 수 있기 때문이다.
-  // -> 따라서 사용자가 사용할 수 있는 메서드를 Interface로 관리할 수 있다.(interface를 활용한 추상화 방법)
-  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
-  maker2.makeCoffee(2);
-
-  // 3. 또 다른 Interface로 타입을 정의해서 인스턴스 생성 => 해당 interface에 있는 메서드만 사용 가능.
-  const maker3: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
-  maker3.fillCoffeeBeans(32);
-  maker3.makeCoffee(2);
-  maker3.clean();
-
   // 4. AmaBarista vs ProBarita => 서로 다른 클래스에 대해 역할이 다르다.
   // CoffeeMaker와 CommercialCoffeeMaker는 가능한 기능이 다름.(Commercial쪽이 더 많음)
   class AmaBaristaUser {
@@ -124,4 +105,31 @@
       this.machine.clean();
     }
   }
+
+  // maker의 Type은 class(CoffeeMachine) or interface(CoffeeMaker)가 될 수 있다.
+  // 1. CoffeeMachine 클래스로 생성한 instance는 모든 메서드(fillCoffeeBeans, makeCoffee)를 활용할 수 있다.
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+  maker.makeCoffee(2);
+
+  // 2. Interface로 추상화 => CoffeeMaker Interface로 타입을 지정한 instance
+  // -> maker2는 fillCoffeeBeans에 접근할 수 없다.
+  // -> Interface에 정의된 메서드에만 접근할 수 있기 때문이다.
+  // -> 따라서 사용자가 사용할 수 있는 메서드를 Interface로 관리할 수 있다.(interface를 활용한 추상화 방법)
+  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  maker2.makeCoffee(2);
+
+  // 3. 또 다른 Interface로 타입을 정의해서 인스턴스 생성 => 해당 interface에 있는 메서드만 사용 가능.
+  const maker3: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  maker3.fillCoffeeBeans(32);
+  maker3.makeCoffee(2);
+  maker3.clean();
+
+  // 4. 프로와 아마추어는 같은 maker를 받지만, 생성자 함수에서 받는 maker의 타입(interface)가 다르다.
+  // 따라서 활용할 수 있는 maker의 메서드가 다르다. 실행 결과도 다르다.
+  const maker4: CoffeeMachine = CoffeeMachine.makeMachine(28);
+  const amateur = new AmaBaristaUser(maker4);
+  const pro = new ProBaristaUser(maker4);
+  // amateur.makeCoffee();
+  pro.makeCoffee();
 }
